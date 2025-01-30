2d5a580f677ddfff7862139c7464003925b4b7f9
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-01-30 11:06:57 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -63,5 +63,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images).*)", "/"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images|sitemap.xml|robots.txt).*)", "/"],
 };
