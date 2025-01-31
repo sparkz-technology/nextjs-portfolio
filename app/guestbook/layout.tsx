f1910963d67190d0cbf4a7847f5fd324eed93f47
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-01-31 14:05:28 +0530

    Update layout.tsx

--- app/guestbook/layout.tsx
+++ app/guestbook/layout.tsx
@@ -2,18 +2,80 @@ import * as FadeIn from "@/components/motion";
 import { AuthButtons } from "@/app/guestbook/auth-buttons";
 import { Metadata } from "next";
 import { auth } from "@/lib/auth";
+import { getSiteMetadata } from "../layout";
 
 interface GuestLayoutProps {
   children: React.ReactNode;
 }
 
-export const metadata: Metadata = {
-  title: "Gustbook",
-  description: "Portfolio gustbook",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const DATA = await getSiteMetadata();
+
+  if (!DATA) {
+    return {
+      metadataBase: new URL("https://example.com"), // Replace with your default URL
+      title: {
+        default: "Next.js Starter",
+        template: "%s | Next.js Starter",
+      },
+      description: "Next.js Starter",
+      openGraph: {
+        title: "Next.js Starter",
+        description: "Next.js Starter",
+        url: "https://example.com", // Replace with your default URL
+        siteName: "Next.js Starter",
+        locale: "en_US",
+        type: "website",
+      },
+      robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+          index: true,
+          follow: true,
+          "max-video-preview": -1,
+          "max-image-preview": "large",
+          "max-snippet": -1,
+        },
+      },
+      twitter: {
+        title: "Next.js Starter",
+        card: "summary_large_image",
+      },
+      verification: {
+        google: "FKMDniF5WlVDC0ppv7xI4TDqbcqLiZjjUH38NJD6B4Q",
+        yandex: "",
+      },
+    };
+  }
+
+  return {
+    title: `Sign My Guestbook | ${DATA.name}`,
+    description: "Leave a message in my portfolio's guestbook. Share your thoughts, feedback, or just say hello!",
+    openGraph: {
+      title: `Sign My Guestbook | ${DATA.name}`,
+      description: "Leave a message in my portfolio's guestbook. Share your thoughts, feedback, or just say hello!",
+      url: `${DATA.url}/guestbook`,
+      siteName: DATA.name!,
+      locale: "en_US",
+      type: "website",
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+  };
+}
 
 export default async function GuestLayout({ children }: GuestLayoutProps) {
-  const session = await auth()
+  const session = await auth();
 
   return (
     <main className="max-w-2xl mx-auto py-12 sm:py-24 px-6">
