00bc6b123ab3d88e7cbd907276da169240df78c2
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-02-05 17:32:15 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,10 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   mode: 'jit',
+  experimental: {
+    optimizeUniversalDefaults: true,
+  },
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
