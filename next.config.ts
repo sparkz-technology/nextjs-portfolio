342969253c5c439d5b05bce699c2e5cdf6bbcc22
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-02-05 18:17:57 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -108,6 +108,9 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   compress:true,
+  cacheHandler: "filesystem",
+  cacheMaxMemorySize: 1048576,
+  cacheDirectory: ".next/cache",
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
