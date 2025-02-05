75b3ad44097bf857734b55b00b5c0567da13a632
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-02-05 18:14:43 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -108,9 +108,6 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   compress:true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
