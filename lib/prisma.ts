1b431ab4793f7891687a6738b57e70a27f562038
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-02-07 12:22:46 +0530

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,5 @@
 
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient().$extends(withAccelerate());
