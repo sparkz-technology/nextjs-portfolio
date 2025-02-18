9282fd061ce98bb07f3097e07d060f21763fe5c2
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-02-18 10:51:38 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,40 @@
+// import { NextResponse } from "next/server";
+// import { NextRequest } from "next/server";
+// import { v4 as uuidv4 } from "uuid";
+// import { auth } from "./lib/auth";
+
+// export const ROOT = "/";
+// export const PUBLIC_ROUTES = ["/", "/guestbook"];
+// export const PRIVATE_ROUTES = [/^\/dashboard\/.*$/];
+// export const DEFAULT_REDIRECT = "/";
+
+// export async function middleware(req: NextRequest) {
+//   const session = await auth();
+//   const { nextUrl } = req;
+//   const isAuthenticated = !!session?.user;
+//   const userRole = session?.user?.role;
+
+//   const isPrivateRoute = PRIVATE_ROUTES.some((route) => route.test(nextUrl.pathname));
+
+//   if (!isAuthenticated && isPrivateRoute) {
+//     console.log("Redirecting unauthenticated user to login page");
+//     return NextResponse.redirect(new URL(ROOT, nextUrl));
+//   }
+
+//   if (isPrivateRoute && userRole !== "SUPER_ADMIN") {
+//     console.log("Redirecting user without SUPER_ADMIN role to dashboard");
+//     return NextResponse.redirect(new URL(ROOT, nextUrl));
+//   }
+
+// return NextResponse.next(); 
+// }
+  
+
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images|sitemap.xml|robots.txt).*)", "/"],
+// };
 import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
-import { v4 as uuidv4 } from "uuid";
+import type { NextRequest } from "next/server";
 import { auth } from "./lib/auth";
 
 export const ROOT = "/";
@@ -8,9 +42,9 @@ export const PUBLIC_ROUTES = ["/", "/guestbook"];
 export const PRIVATE_ROUTES = [/^\/dashboard\/.*$/];
 export const DEFAULT_REDIRECT = "/";
 
-export async function middleware(req: NextRequest) {
+export async function middleware(request: NextRequest) {
   const session = await auth();
-  const { nextUrl } = req;
+  const { nextUrl } = request;
   const isAuthenticated = !!session?.user;
   const userRole = session?.user?.role;
 
@@ -18,18 +52,20 @@ export async function middleware(req: NextRequest) {
 
   if (!isAuthenticated && isPrivateRoute) {
     console.log("Redirecting unauthenticated user to login page");
-    return NextResponse.redirect(new URL(ROOT, nextUrl));
+    return NextResponse.redirect(new URL(ROOT, request.url));
   }
 
   if (isPrivateRoute && userRole !== "SUPER_ADMIN") {
     console.log("Redirecting user without SUPER_ADMIN role to dashboard");
-    return NextResponse.redirect(new URL(ROOT, nextUrl));
+    return NextResponse.redirect(new URL(ROOT, request.url));
   }
 
-return NextResponse.next(); 
+  return NextResponse.next();
 }
-  
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images|sitemap.xml|robots.txt).*)", "/"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|images|sitemap.xml|robots.txt).*)",
+    "/",
+  ],
 };
