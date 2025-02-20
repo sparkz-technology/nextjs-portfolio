0f1213b76cf97771bd635d6cff5bf68ae5d3b3f3
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-02-20 10:45:31 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -28,34 +28,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-    headers: async () => [
-    {
-      source: "/(.*)",
-      headers: [
-        {
-          key: "X-Content-Type-Options",
-          value: "nosniff",
-        },
-        {
-          key: "X-Frame-Options",
-          value: "DENY",
-        },
-        {
-          key: "X-XSS-Protection",
-          value: "1; mode=block",
-        },
-      ],
-    },
-    {
-      source: "/static/(.*)",
-      headers: [
-        {
-          key: "Cache-Control",
-          value: "public, max-age=31536000, immutable",
-        },
-      ],
-    },
-  ],
     generateBuildId: async () => {
     return process.env.VERCEL_GIT_COMMIT_SHA || Date.now().toString()
   },
