b0413374103aa4e70215a780c0a17f9c02000fff
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-03-06 18:39:16 +0530

    Update education-table.tsx

--- app/dashboard/education/education-table.tsx
+++ app/dashboard/education/education-table.tsx
@@ -134,7 +134,7 @@ export default function EducationTable(props: EducationTableProps) {
                   data-id={row.original.id}
                   data-from={row.original.sequenceValue.toString()}
                   data-to={(row.original.sequenceValue - 1).toString()}
-                  disabled={row.original.sequenceValue === 1}
+                  disabled={row.original.sequenceValue === 0}
                 >
                   <ArrowUp size={20} />
                 </Button>
