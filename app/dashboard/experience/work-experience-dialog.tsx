79e986e47c5fa3bd2ecbd6a36f15769014c169b5
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-03-06 19:03:32 +0530

    Update work-experience-dialog.tsx

--- app/dashboard/experience/work-experience-dialog.tsx
+++ app/dashboard/experience/work-experience-dialog.tsx
@@ -5,6 +5,7 @@ import { toast } from "sonner";
 import { format, isToday } from "date-fns";
 import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
 import { AlertTriangle, CalendarIcon, Loader, Loader2 } from "lucide-react";
+import { parse, isValid } from "date-fns";
 
 import { Button } from "@/components/ui/button";
 import {
