524ea43c0071d32e5d2373861e879f1d6d66a7d8
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-03-26 16:12:36 +0530

    Update icon-renderer.tsx

--- components/icon-renderer.tsx
+++ components/icon-renderer.tsx
@@ -44,7 +44,7 @@
 // }
 
 // export default IconRenderer;
-import React from "react";
+import React, { Suspense } from "react";
 import { IconType } from "react-icons";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { MdMailOutline, MdLink } from "react-icons/md";
@@ -65,7 +65,11 @@ const IconRenderer: React.FC<IconRendererProps> = ({ iconName = "" }) => {
   const trimmedIconName = iconName.trim();
   const IconComponent = icons[trimmedIconName] || MdLink; // Default to MdLink if not found
 
-  return <IconComponent size={20} />;
+  return (
+     <Suspense fallback={null}>
+    <IconComponent size={20} />
+           </Suspense>
+       )
 };
 
 export default IconRenderer;
