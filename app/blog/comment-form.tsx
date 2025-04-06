ba6ee4241de5f6fe5f6564073f9e069cf68b6187
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-04-06 22:12:19 +0530

    Update comment-form.tsx

--- app/blog/comment-form.tsx
+++ app/blog/comment-form.tsx
@@ -126,7 +126,7 @@ export function CommentForm({
                                 </span>
                             ) : (
                                 <span className="text-sm font-semibold">
-                                    @{author}
+                                  Replying to @{author}
                                 </span>
                             )
                         }
