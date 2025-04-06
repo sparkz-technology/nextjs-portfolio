9253ce7705870669905fc7ca51a3bf6a5a6d5450
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-04-06 12:14:08 +0000

    refactor: remove unused useEffect import from comment-item component

--- app/blog/comment-item.tsx
+++ app/blog/comment-item.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
